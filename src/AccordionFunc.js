afd4503fc9dec005a4826eccfb944b75025ab104
GoharGhayamyan1995 <gohar.gevorg@mail.ru>
2023-03-30 20:48:06 +0300

    button show_more

--- src/AccordionFunc.js
+++ src/AccordionFunc.js
@@ -1,42 +1,33 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import Pagination from './Pagination';
 import Accordion from './Accordion';
 
 
 function AccordionFunc() {
   const[data,setData]=useState([]);
-  const [currentPage,setCurrentpage]=useState(1);
-  const[postsPerPage,setPostsPerPage]=useState(25);
+ const[numCountries,setNumCountries]=useState(4);
          useEffect(()=>{
       fetch('https://countriesnow.space/api/v0.1/countries/capital')
       .then(res=>res.json())
       .then(res=>setData(res.data));
   },[])
 console.log(data)
-  //Get currentposts
-  
-const indexOfLastPost=currentPage*postsPerPage;
-const indexOfFirstpost=indexOfLastPost-postsPerPage;
-const currentPosts=data.slice(indexOfFirstpost,indexOfLastPost);
-console.log(currentPosts)
 
-
-const paginate=(pageNumber)=>setCurrentpage(pageNumber);
 
   return (
     <main>
     <div className='container'>
       <h3>COUNTRIES AND CAPITALS</h3>
       <section>
-        {data.map((country)=>{
+      {data?.slice(0, numCountries).map((country)=>{
           return <Accordion key={country.name} {...country}/>;
           })}
         
-          <Pagination postsPerpage={postsPerPage} totalPost={data.length} paginate={paginate}/>
+         
       </section>
+      <button className='button' onClick={() => setNumCountries(numCountries + 5)}>see more</button>
     </div>
-    
+   
     </main>
   );
 }
